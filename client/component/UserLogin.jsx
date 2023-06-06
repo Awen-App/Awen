6cb84bc724be569a63e0d717983dfbad904f2731
Tarek-Issaoui <tarek9.9issaoui@gmail.com>
2023-06-06 19:27:41 +0100

    commit to pull

--- client/component/UserLogin.jsx
+++ client/component/UserLogin.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import {View , Text,TextInput,Button,Alert,StyleSheet,TouchableOpacity} from 'react-native'
+import {View , Image,Text,TextInput,Button,Alert,StyleSheet,TouchableOpacity} from 'react-native'
 import {auth} from  "../fireBaseConfig";
 
 import {signInWithEmailAndPassword} from 'firebase/auth'
@@ -21,6 +21,19 @@ const UserLogin = () => {
     }
   return (
     <View style={styles.signin}>
+        <Image
+            style={{ width: 80, height: 80 ,margin:50}}
+            source={require('../assets/awenLogo.png')}
+          />
+            <Text style={styles.wlc}>Sign In to continue </Text>
+            <View style={styles.head}>
+              <TouchableOpacity style={styles.org}>
+                <Text style={styles.appButtonText1}>Organization</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.org}>
+                <Text style={styles.appButtonText1}>Doner</Text>
+              </TouchableOpacity>
+            </View>
         <TextInput
             style={styles.textInput}
             // style={{height: 40}}
@@ -39,7 +52,7 @@ const UserLogin = () => {
             <TouchableOpacity onPress={()=>login()} style={styles.appButtonContainer}>
                 <Text style={styles.appButtonText}>Sign In</Text>
             </TouchableOpacity>
-
+            <Text>Don't have an account? Sign Up</Text>
             {/* <Button
                 title="Log-In"
                 color="#33A09A"
@@ -59,7 +72,7 @@ const styles=StyleSheet.create({
         paddingHorizontal: 20,
       },
     textInput:{
-        width: '100%',
+        width: '80%',
         height: 40,
         borderColor: 'gray',
         borderWidth: 1,
@@ -68,7 +81,7 @@ const styles=StyleSheet.create({
         borderRadius : 8,
     },
     appButtonContainer: {
-        width:'80%',
+        width:'70%',
         elevation: 8,
         backgroundColor: "#009688",
         borderRadius: 10,
@@ -81,6 +94,39 @@ const styles=StyleSheet.create({
         fontWeight: "bold",
         alignSelf: "center",
         textTransform: "uppercase"
+      },
+      head:{
+        flexDirection: 'row',
+        // paddingHorizontal: 20
+        // marginHorizontal:40
+        height:50,
+        marginVertical:40
+        // display:'flex',
+        // justifyContent:'space-between'
+      },
+      org:{
+  
+        width:'45%',
+        backgroundColor: '#009688',
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        borderRadius: 5,
+        marginRight:5,
+        display:'flex',
+        justifyContent:'center',
+        alignItems:'center'
+      },
+      appButtonText1: {
+        fontSize: 15,
+        color: "#fff",
+        fontWeight: "bold",
+        alignSelf: "center",
+        textTransform: "uppercase"
+      },
+      wlc:{
+        // color: '#fff',
+        fontSize: 26,
+        fontWeight: 'bold',
       }
 
 })
--- client/component/UserSignup.jsx
+++ client/component/UserSignup.jsx
@@ -1,41 +1,18 @@
 import React,{useState,useEffect} from 'react'
 // import * as Network from 'expo-network';
-import {View ,Text, TextInput,Button,Alert,StyleSheet,TouchableOpacity} from 'react-native'
+import {View ,Text, Image,TextInput,Button,Alert,StyleSheet,TouchableOpacity} from 'react-native'
 import {auth,googleAuthProvider} from '../fireBaseConfig';
 import { createUserWithEmailAndPassword,signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { useNavigation } from '@react-navigation/native';
 import axios from 'axios';
-// import NetworkInfo from 'react-native-network-info';
 import ADDRESS_IP from '../env.js'
-// import Constants from 'expo-constants';
-// import * as Network from 'expo-network'
-// const {manifest}=Constants;
-// import dotenv from 'react-native-dotenv'
-// import secret from 'react-native-config'
 const UserSignup = () => {
-    console.log(ADDRESS_IP)
-    // const [ipv4Address, setIpv4Address] = useState(null);
-    // console.log(manifest.debuggerHost.split(':').shift())
-    // const [apiAdress,setApiAdress]=useState("");
-    // console.log(Constants)
     const navigation=useNavigation();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confpassword, setConfpassword] = useState('');
-    // const getApiAdress=async()=>{
-    //   const api=await NetworkInfo.NetworkInfo.getIPV4Address();
-    //   console.log(api)
-    //   setApiAdress(api)
-    // }
+    
     const addUser=async()=>{
-      // await getApiAdress();
-      // console.log(apiAdress)
-      // console.log(window.location.host)
-      
-      // NetworkInfo.NetworkInfo.getIPV4Address(ip => {
-      //   setApiAdress(ip); // This will print the IPV4 address
-      // });
-      // console.log(apiAdress)
       const user=await axios.get(`http://${ADDRESS_IP}:3001/users/${email}`)
       if(user.data.length>0){
          Alert.alert("user already exist")
@@ -73,24 +50,22 @@ const UserSignup = () => {
                 });
                   
     }
-
-    // useEffect(() => {
-    //   (async () => {
-    //     const { ipAddress } = await Network.getIpAddressAsync();
-    //     setIpv4Address(ipAddress);
-    //   })();
-    // }, []);
-    // const userSignUpWithGoogle = async () => {
-    //   try {
-    //     const { idToken } = await GoogleSignin.signIn();
-    //     const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-    //     await auth().signInWithCredential(googleCredential);
-    //   } catch (error) {
-    //     console.error('Error signing up with Google:', error);
-    //   }
-    // };
     return (
+      
         <View style={styles.signin}>
+            <Image
+            style={{ width: 80, height: 80 ,margin:50}}
+            source={require('../assets/awenLogo.png')}
+          />
+            <Text style={styles.wlc}>Sign up to continue </Text>
+            <View style={styles.head}>
+              <TouchableOpacity style={styles.org}>
+                <Text style={styles.appButtonText1}>Organization</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.org}>
+                <Text style={styles.appButtonText1}>Doner</Text>
+              </TouchableOpacity>
+            </View>
             <TextInput
             style={styles.textInput}
             placeholder="E-mail"
@@ -106,18 +81,18 @@ const UserSignup = () => {
             />
             <TextInput
                 style={styles.textInput}
-                // style={{height: 40}}
                 placeholder="Confirm Password"
                 onChangeText={newText => setConfpassword(newText)}
                 defaultValue={confpassword}
                 secureTextEntry={true}
             />
             <TouchableOpacity onPress={()=>addUser()} style={styles.appButtonContainer}>
-                <Text style={styles.appButtonText}>Sign-Up</Text>
+                <Text style={styles.appButtonText}>Sign Up</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.appButtonContainer} >
               <Text style={styles.appButtonText}>Sign up with Google</Text>
             </TouchableOpacity>
+            <Text>Already have an account? Sign In</Text>
         </View>
   )
 }
@@ -127,13 +102,14 @@ export default UserSignup;
 
 const styles=StyleSheet.create({
   signin: {
-      flex: 0.7,
+      
       alignItems: 'center',
       justifyContent: 'center',
       paddingHorizontal: 20,
+      // backgroundColor:'red'
     },
   textInput:{
-      width: '100%',
+      width: '80%',
       height: 40,
       borderColor: 'gray',
       borderWidth: 1,
@@ -143,7 +119,7 @@ const styles=StyleSheet.create({
   },
   appButtonContainer: {
       margin:10,
-      width:'80%',
+      width:'70%',
       elevation: 8,
       backgroundColor: "#009688",
       borderRadius: 10,
@@ -166,6 +142,39 @@ const styles=StyleSheet.create({
     buttonText: {
       color: '#fff',
       fontSize: 16,
+      fontWeight: 'bold',
+    },
+    head:{
+      flexDirection: 'row',
+      // paddingHorizontal: 20
+      // marginHorizontal:40
+      height:50,
+      marginVertical:40
+      // display:'flex',
+      // justifyContent:'space-between'
+    },
+    org:{
+
+      width:'45%',
+      backgroundColor: '#009688',
+      paddingHorizontal: 20,
+      paddingVertical: 10,
+      borderRadius: 5,
+      marginRight:5,
+      display:'flex',
+      justifyContent:'center',
+      alignItems:'center'
+    },
+    appButtonText1: {
+      fontSize: 15,
+      color: "#fff",
+      fontWeight: "bold",
+      alignSelf: "center",
+      textTransform: "uppercase"
+    },
+    wlc:{
+      // color: '#fff',
+      fontSize: 26,
       fontWeight: 'bold',
     }
 })
