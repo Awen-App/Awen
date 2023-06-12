cf3f60d1533e0a81699e57d3a76a704d0fdd5fff
Amrou-Alwane <amroualwane1@gmail.com>
2023-06-12 13:23:16 +0200

    done with merge

--- client/App.js
+++ client/App.js
@@ -12,6 +12,9 @@ import UserLogin from './component/UserLogin';
 import SignInOrganization from './component/organization/SignIn';
 import UserSignup from './component/UserSignup';
 import AuthOrganization from './component/organization/AuthOrganization';
+import Profile from './component/organization/Profile';
+import Modify from './component/organization/ModifyOrg';
+import NavbarOrganization from './component/organization/NavbarOrganization';
 const Stack=createNativeStackNavigator();
 export default App=()=>{
     return (
@@ -21,7 +24,10 @@ export default App=()=>{
             <Stack.Screen name= "CauseDetails" component={CauseDetail} options={{ headerTitle: () => <View /> }} />
             <Stack.Screen name="UserSignup" component={UserSignup} options={{ headerShown: false }}/>
             <Stack.Screen name="OrganizationSignUp" component={AuthOrganization} options={{ headerShown: false }}/>
-            <Stack.Screen name="OrganizationLogin" component={SignInOrganization} options={{ headerShown: false }}/>
+            <Stack.Screen name= "organizationHome" component={NavbarOrganization} options={{ headerShown:false}}/>
+            <Stack.Screen name="OrganizationLogin" component={SignInOrganization} options={{ headerShown: false }}/>       
+            <Stack.Screen name= "ModifyOrganization" component={Modify} options={{ headerShown:false}}/>
+            <Stack.Screen name= "profile" component={Profile} options={{ headerShown:false}}/> 
             <Stack.Screen name="UserSignin" component={UserLogin} options={{ headerShown: false }}/>
             <Stack.Screen name= "AllCauses" component={AllCauses} options={{ headerTitle: () => <View /> }}/>
             <Stack.Screen name='MyStack' component={MyStack} options={{ headerTitle: () => <View />}}/>
